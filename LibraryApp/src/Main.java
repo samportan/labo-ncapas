b192902b942e79c99a4d13cf668065c403e442be
samportan <50893014+samportan@users.noreply.github.com>
2025-04-05 10:27:07 -0600

    create console menu

--- LibraryApp/src/Main.java
+++ LibraryApp/src/Main.java
@@ -1,15 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private static List<Libro> libros = new ArrayList<>();
+    //TODO: List of ventas
+    private static Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+
+        // Crear algunos libros
+        Libro libro1 = new Libro("1", "El Alquimista", "Paulo Coelho", 10.99, 100);
+        Libro libro2 = new Libro("2", "Cien años de soledad", "Gabriel García Márquez", 12.50, 200);
+        Libro libro3 = new Libro("3", "1984", "George Orwell", 15.00, 150);
+
+
+        // Agregar libros a la lista
+        libros.add(libro1);
+        libros.add(libro2);
+        libros.add(libro3);
+
+        //TODO: Create Ventas
+
+        //Console menu
+        int opcion = 0;
+
+        while (opcion != 6) {
+            mostrarMenu();
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    registrarLibro();
+                    break;
+                case 2:
+                    registrarVenta();
+                    break;
+                case 3:
+                    calcularLibroMasVendido();
+                    break;
+                case 4:
+                    mostarLibroConVentasSuperores(100);
+                    break;
+                case 5:
+                    actualizarVentas();
+                    break;
+                case 6:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+            }
         }
     }
+
+    private static void mostrarMenu() {
+        System.out.println("\n--- Menú de Gestión de Librería ---");
+        System.out.println("1. Registrar un nuevo libro");
+        System.out.println("2. Registrar una venta");
+        System.out.println("3. Ver el libro más vendido");
+        System.out.println("4. Ver libros con más de 500 unidades vendidas");
+        System.out.println("5. Actualizar estadísticas de ventas");
+        System.out.println("6. Salir");
+        System.out.print("Elige una opción: ");
+    }
+
+    public static void registrarLibro() {
+
+    }
+
+    public static void registrarVenta() {
+
+    }
+
+    public static void calcularLibroMasVendido() {}
+
+    public static void mostarLibroConVentasSuperores(int x) {}
+
+    public static void actualizarVentas() {}
 }
