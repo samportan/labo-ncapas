ff51deace44f6f99e4816fc8a2431ee9d1bf9c74
samportan <50893014+samportan@users.noreply.github.com>
2025-04-05 10:01:49 -0600

    Added Libro class

---
+++ LibraryApp/src/Libro.java
@@ -0,0 +1,64 @@
+public class Libro {
+    private String id;
+    private String titulo;
+    private String autor;
+    private double precio;
+    private int ventasTotales;
+
+    public Libro(String id, String titulo, String autor, double precio, int ventasTotales) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.precio = precio;
+        this.ventasTotales = ventasTotales;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getVentasTotales() {
+        return ventasTotales;
+    }
+
+    public void setVentasTotales(int ventasTotales) {
+        this.ventasTotales = ventasTotales;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public void actualizarVentas(int cantidad) {
+        this.ventasTotales += cantidad;
+    }
+}
