c230ec19cf2ea2fb38b2047782b0be51934f03da
samportan <50893014+samportan@users.noreply.github.com>
2025-04-06 16:59:43 -0600

    Added update sales function

--- LibraryApp/src/Main.java
+++ LibraryApp/src/Main.java
@@ -15,6 +15,13 @@ public class Main {
         libros.add(new Libro("1", "El Alquimista", "Paulo Coelho", 10.99, 99));
         libros.add(new Libro("2", "Cien años de soledad", "Gabriel García Márquez", 12.50, 200));
         libros.add(new Libro("3", "1984", "George Orwell", 15.00, 150));
+        libros.add(new Libro("4", "El Principito", "Antoine de Saint-Exupéry", 8.99, 50));
+        libros.add(new Libro("5", "El Hobbit", "J.R.R. Tolkien", 20.00, 300));
+        libros.add(new Libro("6", "Crónica de una muerte anunciada", "Gabriel García Márquez", 10.00, 120));
+        libros.add(new Libro("7", "El amor en los tiempos del cólera", "Gabriel García Márquez", 14.99, 80));
+        libros.add(new Libro("8", "El túnel", "Ernesto Sabato", 9.99, 60));
+        libros.add(new Libro("9", "Rayuela", "Julio Cortázar", 11.50, 90));
+        libros.add(new Libro("10", "La casa de los espíritus", "Isabel Allende", 13.50, 110));
 
         //TODO: Create Ventas
 
@@ -38,7 +45,7 @@ public class Main {
                     System.out.println("El libro con más ventas es: " + max.getTitulo() + " con " +  max.getVentasTotales() + " ventas.");
                     break;
                 case 4:
-                    mostarLibroConVentasSuperores();
+                    mostarLibroConVentasSuperiores();
                     break;
                 case 5:
                     actualizarVentas();
@@ -72,7 +79,7 @@ public class Main {
 
     }
 
-    public static void mostarLibroConVentasSuperores() {
+    public static void mostarLibroConVentasSuperiores() {
         System.out.println("Introduce el número de ventas:");
         int x = sc.nextInt();
         System.out.println("Libros con más de " + x + " ventas:");
@@ -81,5 +88,14 @@ public class Main {
                 .forEach(libro -> System.out.println(libro.getTitulo() + " - ventas: " + libro.getVentasTotales()));
     }
 
-    public static void actualizarVentas() {}
+    public static void actualizarVentas() {
+        for (Venta venta : ventas) {
+            venta.getLibro().actualizarVentas(venta.getCantidadVendida());
+        }
+
+        System.out.println("Dato de ventas actualizadas: ");
+        for (Libro libro: libros) {
+            System.out.println(libro.getTitulo() + " - ventas: " + libro.getVentasTotales());
+        }
+    }
 }
